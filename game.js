2eb204a8f3f03d051257b6de01d2e4cf5e085d19
k-cormack <k.cormack@gmail.com>
2018-08-03 16:49:16 -0600

    COB Push

--- game.js
+++ game.js
@@ -1,18 +1,68 @@
+
+
 let health = 100
 
-function slap(){
+let fighterName = 'Italion Stallion'
+
+let hits = 0
+
+// function assignName() {
+//     document.getElementById("player-name").innerText = fighterName;
+// }
+// assignName()
+
+let weapons = {
+
+    chair: {
+        name: "Chair",
+        modifier: 1,
+        description: "Have a seat!!"
+    },
+
+    chain: {
+        name: "Chain",
+        modifier: 3,
+        description: "Bring the pain!!"
+    },
+
+    lightSaber: {
+        name: "Light Saber",
+        modifier: 6,
+        description: "Han shot first!!"
+    }
+}
+
+let italionStallion = {
+    health: 100,
+    name: "Italian Stallion",
+    items: []
+}
+
+let sugarRay = {
+    health: 120,
+    name: "Sugar Ray",
+    items: []
+}
+
+function slap() {
     health--;
-    document.getElementById("health").innerText = health;
+    update()
 }
 
 function punch() {
     health -= 5;
-    document.getElementById("health").innerText = health;
+    update()
 }
 
 function kick() {
     health -= 10;
+    update()
+}
+
+function update() {
+    hits++;
     document.getElementById("health").innerText = health;
+    document.getElementById("hits").innerText = hits
 }
 
 // function update() {
