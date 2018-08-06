dfc64273ce51bd3f221747f49a945c4c4e48b196
k-cormack <k.cormack@gmail.com>
2018-08-05 23:04:28 -0600

    basic working changes

--- game.js
+++ game.js
@@ -2,70 +2,148 @@
 
 let health = 100
 
-let fighterName = 'Italion Stallion'
-
 let hits = 0
+
+let italianStallion = new fighterType('Italian Stallion', 100, 1, 5, 10)
+
+let sugarRay = new fighterType('Sugar Ray', 100, 1, 5, 10)
+
+let fighter = italianStallion
+
+let multiplier = 1
 
 // function assignName() {
 //     document.getElementById("player-name").innerText = fighterName;
 // }
 // assignName()
 
-let weapons = {
 
-    chair: {
-        name: "Chair",
-        modifier: 1,
-        description: "Have a seat!!"
-    },
 
-    chain: {
-        name: "Chain",
-        modifier: 3,
-        description: "Bring the pain!!"
-    },
-
-    lightSaber: {
-        name: "Light Saber",
-        modifier: 6,
-        description: "Han shot first!!"
+function fighterType(name, health, slap, punch, kick) {
+    this.name = name
+    this.health = health
+    this.blows = {
+        slap, punch, kick
     }
 }
 
-let italionStallion = {
-    health: 100,
-    name: "Italian Stallion",
-    items: []
-}
+// function toggleFighter() { }
 
-let sugarRay = {
-    health: 120,
-    name: "Sugar Ray",
-    items: []
-}
+// function weapon(name, modifier, description) {
+//     this.name = name;
+//     this.modifier = modifier;
+//     this.description = description;
+// }
+
+// let weaponType = {
+//     chair: new weapon("Chair", 1.5, "Have a seat!!"),
+//     chain: new weapon("Chain", 2, "Bring the pain!!"),
+//     lightSaber: new weapon("Light Saber", 3, "Han shot first!!")
+// }
+
+
+let checkedWeapon = document.getElementsByTagName('input').values
+
+// let weaponValue;
+// for (var i = 0, i < checkedWeapon.length; i++) {
+//     if (checkedWeapon[i].type === 'radio' && checkedWeapon[i].checked) {
+//         weaponValue = checkedWeapon[i].weaponValue;
+//     }
+// }
+
+// var radios = document.getElementsByTagName('input');
+// var value;
+// for (var i = 0; i < radios.length; i++) {
+//     if (radios[i].type === 'radio' && radios[i].checked) {
+//         value = radios[i].value;       
+//     }
+// }
+
+
 
 function slap() {
-    health--;
+    multiplierFunction();
+    health = health - 1 - multiplier;
     update()
 }
 
 function punch() {
-    health -= 5;
+    multiplierFunction();
+    health = health - 5 - multiplier;
     update()
 }
 
 function kick() {
-    health -= 10;
+    multiplierFunction();
+    health = health - 10 - multiplier;
     update()
 }
 
 function update() {
     hits++;
+    if (health <= 0) {
+        health = 0
+    }
     document.getElementById("health").innerText = health;
     document.getElementById("hits").innerText = hits
 }
 
-// function update() {
-//     // document.getElementById("health").innerText = health;
 
-// }
+
+
+
+function clearRadio() {
+    var clear = document.getElementsByName("weapon-group");
+    for (var i = 0; i < clear.length; i++) {
+        clear[i].checked = false; //I have NO IDEA why this is red-squiggled - had to assign "checked : false" to each element in HTML
+    }
+}
+//     var clearRadio = document.getElementsByTagName('input');
+//     // var clearRadio = document.getElementById('chair-box')
+//     for (var i = 0; i < clearRadio.length; i++) {
+//         if (clearRadio[i].type == 'checkbox') {
+//             clearRadio[i].checked = false;
+//         }
+//     }
+
+
+function reset() {
+    health = 100;
+    hits = 0;
+    multiplier = 0;
+    document.getElementById('health').innerText = health;
+    document.getElementById("hits").innerText = hits;
+    document.getElementById("description").innerText = "";
+    clearRadio()
+
+}
+function multiplierFunction() {
+    let chair = document.getElementById("radio-chair").checked; //Why the red-squiggly? It works.....
+
+    let chain = document.getElementById("radio-chain").checked;
+
+    let lightSaber = document.getElementById("radio-lightsaber").checked;
+
+
+
+    if (chair == true) {
+        multiplier = 1.5;
+        document.getElementById("description").innerText = "Have a seat!!"
+    }
+    else if (chain == true) {
+        multiplier = 2
+        document.getElementById("description").innerText = "Chain bringin' the pain!"
+    }
+
+    else if (lightSaber == true) {
+        multiplier = 3
+        document.getElementById("description").innerText = "Han shot first!!!!"
+    }
+
+    else {
+        multiplier = 0;
+        document.getElementById("description").innerText = "Bare knuckle brawlin'!!"
+    }
+}
+
+
